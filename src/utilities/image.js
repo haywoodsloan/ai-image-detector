596ab0e9d370cd92c42b8bf3c76e2817df466a44
Sloan Haywood <haywood.sloan@yahoo.com>
2024-04-26 01:50:20 -0400

    updates

--- src/utilities/image.js
+++ src/utilities/image.js
@@ -1,11 +1,20 @@
+import { openAsBlob } from 'fs';
+
 /**
  * @param {string} url
  */
 export async function getImageAsBlob(url) {
-  const response = await fetch(url, {
-    headers: {
-      Accept: 'image/*',
-    },
-  });
-  return await response.blob();
+  const { protocol } = new URL(url);
+  if (['http:', 'https:'].includes(protocol)) {
+    const response = await fetch(url, {
+      headers: {
+        Accept: 'image/*',
+      },
+    });
+
+    return await response.blob();
+  }
+
+  const blob = await openAsBlob(url);
+  return blob;
 }
