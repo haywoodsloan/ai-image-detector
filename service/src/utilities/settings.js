5b17d5253369d13f8d8be31c40fbd053880c3298
Sloan Haywood <haywood.sloan@yahoo.com>
2024-04-26 23:21:06 -0400

    update script

--- service/src/utilities/settings.js
+++ service/src/utilities/settings.js
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
 
 export async function loadLocalSettings() {
-  const settingsPath = new URL("../../local.settings.json", import.meta.url);
+  const settingsPath = new URL("../../settings.local.json", import.meta.url);
   const buffer = await fs.readFile(settingsPath);
   const settings = JSON.parse(buffer);
   Object.assign(process.env, settings);
