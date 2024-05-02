da2932287a3ed0afea0043e014436e4baa67e8c0
Sloan Haywood <haywood.sloan@yahoo.com>
2024-05-02 17:37:18 -0400

    only use my model

--- service/src/constants.js
+++ service/src/constants.js
@@ -2,6 +2,5 @@ export const AiClassLabel = 'artificial';
 export const AiClassThresh = 0.95;
 
 export const DetectorModels = Object.freeze([
-  'umm-maybe/AI-image-detector',
   'haywoodsloan/autotrain-ai-image-detect',
 ]);
