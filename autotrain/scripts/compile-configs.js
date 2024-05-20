83de59f5c4be4527f2618f113a4081aaa6920342
Sloan Haywood <haywood.sloan@yahoo.com>
2024-05-19 23:07:46 -0400

    compile script will auto refresh

--- autotrain/scripts/compile-configs.js
+++ autotrain/scripts/compile-configs.js
@@ -3,6 +3,10 @@ import { mkdir, readFile, rm, stat, watch, writeFile } from 'fs/promises';
 import Handlebars from 'handlebars';
 import { dirname, join } from 'path';
 
+const AutoRefreshInterval = 5 * 60 * 1000;
+const CompilePath = '.compiled/';
+const ConfigPath = 'config/';
+
 Handlebars.registerHelper('datetime', () => {
   const date = new Date();
 
@@ -16,21 +20,29 @@ Handlebars.registerHelper('datetime', () => {
   return `${year}${month}${day}-${hour}${minute}`;
 });
 
-const compilePath = '.compiled/';
-const compileConfigPath = join(compilePath, 'config/');
+/** @type {Set<string>} */
+const refreshConfigs = new Set();
+
+const compileConfigPath = join(CompilePath, ConfigPath);
 await rm(compileConfigPath, { force: true, recursive: true });
 
-const configPath = 'config/';
-const configs = await getFilesFromDir(configPath);
+const configs = await getFilesFromDir(ConfigPath);
 for (const config of configs) {
   console.log(`Compiling ${config}`);
   await compile(config);
 }
 
+// Recompile periodically support datetime updates
+setTimeout(async () => {
+  for (const config of refreshConfigs) {
+    await compile(config);
+  }
+}, AutoRefreshInterval);
+
 console.log('Watching for config changes...');
-const watcher = watch(configPath, { recursive: true });
+const watcher = watch(ConfigPath, { recursive: true });
 for await (const { filename } of watcher) {
-  const filePath = join(configPath, filename);
+  const filePath = join(ConfigPath, filename);
   try {
     const fileStats = await stat(filePath);
     if (!fileStats.isFile()) continue;
@@ -38,16 +50,23 @@ for await (const { filename } of watcher) {
     console.log(`Changed ${filePath}`);
     await compile(filePath);
   } catch {
+    refreshConfigs.delete(filePath);
     console.log(`Removed ${filePath}`);
-    await rm(join(compilePath, filePath), { force: true, recursive: true });
+    await rm(join(CompilePath, filePath), { force: true, recursive: true });
   }
 }
 
 async function compile(filename) {
-  const template = Handlebars.compile(await readFile(filename, 'utf8'));
+  const fileContent = await readFile(filename, 'utf8');
+
+  const hasDatetime = /{{\s*datetime\s*}}/.test(fileContent);
+  if (hasDatetime) refreshConfigs.add(filename);
+  else refreshConfigs.delete(filename);
+
+  const template = Handlebars.compile(fileContent);
   const compiled = template();
 
-  const outputPath = join(compilePath, filename);
+  const outputPath = join(CompilePath, filename);
   const parentPath = dirname(outputPath);
 
   await mkdir(parentPath, { recursive: true });
