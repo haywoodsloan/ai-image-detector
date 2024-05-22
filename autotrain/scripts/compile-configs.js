ab660127458c37e87c7fc5aff5cf06a219dbb4bd
Sloan Haywood <haywood.sloan@yahoo.com>
2024-05-22 02:21:42 -0400

    revise config system

--- autotrain/scripts/compile-configs.js
+++ autotrain/scripts/compile-configs.js
@@ -1,11 +1,16 @@
-import { getFilesFromDir } from 'common/utilities/files.js';
 import { mkdir, readFile, rm, stat, watch, writeFile } from 'fs/promises';
 import Handlebars from 'handlebars';
 import { dirname, join } from 'path';
+import YAML from 'yaml';
 
+const DatetimeRegex = /{{\s*datetime\s*}}/;
 const AutoRefreshInterval = 5 * 60 * 1000;
+
 const CompilePath = '.compiled/';
 const ConfigPath = 'config/';
+
+const BasePath = join(ConfigPath, 'base.yml.hbs');
+const ModelsPath = join(ConfigPath, 'models.yml');
 
 Handlebars.registerHelper('datetime', () => {
   const date = new Date();
@@ -25,50 +30,94 @@ const refreshConfigs = new Set();
 
 const compileConfigPath = join(CompilePath, ConfigPath);
 await rm(compileConfigPath, { force: true, recursive: true });
-
-const configs = await getFilesFromDir(ConfigPath);
-for (const config of configs) {
-  console.log(`Compiling ${config}`);
-  await compile(config);
-}
+await compileAll();
 
 // Recompile periodically to support datetime updates
 setInterval(async () => {
-  for (const config of refreshConfigs) {
-    await compile(config);
+  if (refreshConfigs.has(BasePath)) {
+    await compileAll();
+  } else {
+    for (const config of refreshConfigs) await compile(config);
   }
 }, AutoRefreshInterval);
 
-console.log('Watching for config changes...');
+console.log('Configs successfully compiled, watching for changes...');
 const watcher = watch(ConfigPath, { recursive: true });
-for await (const { filename } of watcher) {
-  const filePath = join(ConfigPath, filename);
-  try {
-    const fileStats = await stat(filePath);
-    if (!fileStats.isFile()) continue;
+for await (const { filename, eventType } of watcher) {
+  if (eventType !== 'change') continue;
 
-    console.log(`Changed ${filePath}`);
-    await compile(filePath);
-  } catch {
-    refreshConfigs.delete(filePath);
-    console.log(`Removed ${filePath}`);
-    await rm(join(CompilePath, filePath), { force: true, recursive: true });
+  const filePath = join(ConfigPath, filename);
+  const fileStats = await stat(filePath);
+  if (!fileStats.isFile()) continue;
+
+  if (filePath === BasePath) {
+    console.log('Base config changed, recompiling all');
+    await compileAll();
+  } else if (filePath === ModelsPath) {
+    console.log('Model list changed, recompiling all');
+    await compileAll();
+  } else {
+    console.log(`Recompiling ${filePath}`);
+    await compile(filename);
   }
 }
 
-async function compile(filename) {
-  const fileContent = await readFile(filename, 'utf8');
+async function compileAll() {
+  const baseTemplate = await getBaseTemplate();
 
-  const hasDatetime = /{{\s*datetime\s*}}/.test(fileContent);
-  if (hasDatetime) refreshConfigs.add(filename);
-  else refreshConfigs.delete(filename);
+  const modelsContent = await readFile(ModelsPath, 'utf8');
+  const models = YAML.parse(modelsContent);
 
-  const template = Handlebars.compile(fileContent);
-  const compiled = template();
+  for (const model of models) {
+    const baseContent = baseTemplate({ model });
+    await compile(`${model}.yml`, YAML.parse(baseContent));
+  }
+}
 
-  const outputPath = join(CompilePath, filename);
+/**
+ * @param {string} modelPath
+ * @param {any} baseConfig
+ */
+async function compile(modelPath, baseConfig = null) {
+  if (!baseConfig) {
+    const baseTemplate = await getBaseTemplate();
+    const baseContent = baseTemplate({ model: modelPath });
+    baseConfig = YAML.parse(baseContent);
+  }
+
+  const fileName = join(ConfigPath, modelPath);
+  const merged = {};
+
+  try {
+    const fileContent = await readFile(fileName, 'utf8');
+    const hasDatetime = DatetimeRegex.test(fileContent);
+
+    if (hasDatetime) refreshConfigs.add(modelPath);
+    else refreshConfigs.delete(modelPath);
+
+    const template = Handlebars.compile(fileContent);
+    const compiled = template();
+
+    const parsed = YAML.parse(compiled);
+    Object.assign(merged, baseConfig, parsed);
+  } catch {
+    Object.assign(merged, baseConfig);
+  }
+
+  const output = YAML.stringify(merged);
+  const outputPath = join(CompilePath, fileName);
   const parentPath = dirname(outputPath);
 
   await mkdir(parentPath, { recursive: true });
-  await writeFile(outputPath, compiled);
+  await writeFile(outputPath, output);
+}
+
+async function getBaseTemplate() {
+  const baseContent = await readFile(BasePath, 'utf8');
+  const baseHasDatetime = DatetimeRegex.test(baseContent);
+
+  if (baseHasDatetime) refreshConfigs.add(BasePath);
+  else refreshConfigs.delete(BasePath);
+
+  return Handlebars.compile(baseContent);
 }
