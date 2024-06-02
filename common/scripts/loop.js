fa550659c13379f909f12d3277b54fb5ac9be37d
Sloan Haywood <haywood.sloan@yahoo.com>
2024-06-02 15:40:18 -0400

    update loop script

--- common/scripts/loop.js
+++ common/scripts/loop.js
@@ -3,36 +3,40 @@ import { spawn } from 'child_process';
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-import { wait } from '../src/utilities/sleep.js';
 import { y } from '../src/utilities/colors.js';
+import { wait } from '../src/utilities/sleep.js';
 
-const args = await yargs(hideBin(process.argv))
-  .command('$0 <cmd>', 'loop command', (yargs) =>
-    yargs
-      .option('count', {
-        type: 'number',
-        description: 'The maximum number of loops',
-        default: Infinity,
-      })
-      .option('timeout', {
-        type: 'number',
-        description: 'Timeout between loops in seconds',
-        default: 0,
-      })
-      .positional('cmd', {
-        type: 'string',
-        description: 'The command to execute on a loop',
-        demandOption: true,
-      })
+await yargs(hideBin(process.argv))
+  .command(
+    '$0 <cmd>',
+    false,
+    (yargs) =>
+      yargs
+        .option('count', {
+          type: 'number',
+          description: 'The maximum number of loops',
+          default: Infinity,
+        })
+        .option('timeout', {
+          type: 'number',
+          description: 'Timeout between loops in seconds',
+          default: 0,
+        })
+        .positional('cmd', {
+          type: 'string',
+          description: 'The command to execute on a loop',
+          demandOption: true,
+        }),
+    async (args) => {
+      let count = 0;
+      while (count < args.count) {
+        const child = spawn(args.cmd, { shell: true, stdio: 'inherit' });
+        await new Promise((res) => child.once('close', res));
+
+        console.log(y`\nWaiting for ${args.timeout} seconds...`);
+        await wait(args.timeout * 1000);
+        count++;
+      }
+    }
   )
   .parse();
-
-let count = 0;
-while (count < args.count) {
-  const child = spawn(args.cmd, { shell: true, stdio: 'inherit' });
-  await new Promise((res) => child.once('close', res));
-
-  console.log(y`\nWaiting for ${args.timeout} seconds...`);
-  await wait(args.timeout * 1000);
-  count++;
-}
