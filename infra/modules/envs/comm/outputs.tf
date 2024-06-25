701bcde18a4901c9206167f2a32edecc5ce4e905
Sloan Haywood <haywood.sloan@yahoo.com>
2024-06-25 00:48:14 -0400

    make connection strings sensitive

--- infra/modules/envs/comm/outputs.tf
+++ infra/modules/envs/comm/outputs.tf
@@ -1,7 +1,9 @@
 output "comm_service_connection_string" {
-  value = azurerm_communication_service.comm_service.primary_connection_string
+  sensitive = true
+  value     = azurerm_communication_service.comm_service.primary_connection_string
 }
 
 output "comm_service_secondary_connection_string" {
-  value = azurerm_communication_service.comm_service.secondary_connection_string
+  sensitive = true
+  value     = azurerm_communication_service.comm_service.secondary_connection_string
 }
