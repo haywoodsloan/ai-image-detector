3dd77596d957ae56730084ee65ec85781ca4e658
Sloan Haywood <haywood.sloan@yahoo.com>
2024-12-31 17:14:54 -0500

    Fixed memory overflows when scrapping reddit

--- scrapper/src/index.js
+++ scrapper/src/index.js
@@ -89,13 +89,10 @@ const LoaderSelector =
 const LoadingSelector =
   'shreddit-feed faceplate-partial[id^="partial-more-posts"][hasBeenLoaded]';
 const RetrySelector = 'shreddit-feed button ::-p-text(Retry)';
-const CleanupSelector =
-  'shreddit-feed article, shreddit-feed shreddit-ad-post, shreddit-feed hr';
 const ImageSelector =
   'shreddit-post img[src^="https://preview.redd.it"]:not([alt=""])';
 
 const TestRatio = 0.1;
-const CleanupRemainder = 9;
 const RetryLimit = 10;
 const MaxPendingUploads = 5;
 
@@ -123,57 +120,60 @@ for (const url of urls) scrappedUrls.add(url);
 // Track the total number of images scrapped
 let count = 0;
 
-// Fetch the list of images from the National Gallery of Art
-console.log(y`Fetching image list from the National Gallery of Art`);
-const ngoaRequest = await fetch(NgoaImageCsv);
+{
+  // Fetch the list of images from the National Gallery of Art
+  console.log(y`Fetching image list from the National Gallery of Art`);
+  const ngoaRequest = await fetch(NgoaImageCsv);
 
-if (!ngoaRequest.ok) {
-  const error = `Status: ${ngoaRequest.statusText || ngoaRequest.status}`;
-  console.log(r`Fetch failed image list, skipping to next source (${error})`);
-} else {
-  // Pipe the NGoA request through the CSV parser
-  const ngoaRecords = parse(await ngoaRequest.text(), {
-    columns: true,
-    skip_empty_lines: true,
-  });
+  if (!ngoaRequest.ok) {
+    const error = `Status: ${ngoaRequest.statusText || ngoaRequest.status}`;
+    console.log(r`Fetch failed image list, skipping to next source (${error})`);
+  } else {
+    // Pipe the NGoA request through the CSV parser
+    const ngoaRecords = parse(await ngoaRequest.text(), {
+      columns: true,
+      skip_empty_lines: true,
+    });
 
-  // Iterate over each image from the NGoA
-  console.log(g`Finished fetching image list`);
-  for (const media of ngoaRecords) {
-    if (count >= args.count) break;
-    const imageUrl = new URL(`${media.iiifurl}/full/max/0/default.jpg`);
+    // Iterate over each image from the NGoA
+    console.log(g`Finished fetching image list`);
+    for (const media of ngoaRecords) {
+      if (count >= args.count) break;
+      const imageUrl = new URL(`${media.iiifurl}/full/max/0/default.jpg`);
 
-    // Skip urls to images that have already been scrapped
-    if (scrappedUrls.has(imageUrl.toString())) continue;
-    scrappedUrls.add(imageUrl.toString());
+      // Skip urls to images that have already been scrapped
+      if (scrappedUrls.has(imageUrl.toString())) continue;
+      scrappedUrls.add(imageUrl.toString());
 
-    queueValidation(imageUrl, RealLabel);
-    if (validationQueue.size >= UploadBatchSize) {
-      await queueUpload();
+      queueValidation(imageUrl, RealLabel);
+      if (validationQueue.size >= UploadBatchSize) {
+        await queueUpload();
+      }
+
+      if (pendingUploads.size >= MaxPendingUploads) {
+        await throttleUploads();
+      }
     }
 
-    if (pendingUploads.size >= MaxPendingUploads) {
-      await throttleUploads();
-    }
+    // All done with NGoA images
+    console.log(g`Finished fetching images from National Gallery of Art`);
   }
 }
 
 // Fetch the list of images from Pexels for each API endpoint
 for (const pexelsApi of PexelsApis) {
-  console.log(y`Fetching first page of images from Pexels (${pexelsApi})`);
+  console.log(y`Fetching images from Pexels (${pexelsApi})`);
   const pexelsRequest = await fetch(pexelsApi, {
     headers: { Authorization: PEXELS_KEY },
   });
 
   if (!pexelsRequest.ok) {
     const error = `Status: ${pexelsRequest.statusText || pexelsRequest.status}`;
-    console.log(r`Pexels request failed, skipping to next source (${error})`);
+    console.log(r`Request failed, skipping to next source (${error})`);
     continue;
   }
 
   let { photos, next_page } = await pexelsRequest.json();
-  console.log(g`Finished fetching first page of images`);
-
   while (photos.length && count < args.count) {
     const photo = photos.pop();
     const photoUrl = new URL(photo.src.original);
@@ -193,7 +193,6 @@ for (const pexelsApi of PexelsApis) {
     }
 
     if (!photos.length && next_page) {
-      console.log(y`Fetching next page of images from Pexels (${next_page}`);
       const nextRequest = await fetch(next_page, {
         headers: { Authorization: PEXELS_KEY },
       });
@@ -202,143 +201,152 @@ for (const pexelsApi of PexelsApis) {
         const error = `Status: ${nextRequest.statusText || nextRequest.status}`;
         console.log(r`Request failed, skipping to next source (${error})`);
         break;
-      }
-
-      ({ photos, next_page } = await nextRequest.json());
-      console.log(g`Finished fetching next page of images`);
+      } else ({ photos, next_page } = await nextRequest.json());
     }
   }
 }
 
-// Launch Puppeteer
-const browser = await launch({
-  headless: !args.debug,
-  defaultViewport: { width: WindowWidth, height: WindowHeight },
-  args: [`--window-size=${WindowWidth},${WindowHeight}`],
-});
+// Done with this Pexels API
+console.log(g`Done fetching images from Pexels`);
 
-const [page] = await browser.pages();
-await page.setUserAgent(ChromeUA);
+{
+  // Launch Puppeteer
+  const browser = await launch({
+    headless: !args.debug,
+    defaultViewport: { width: WindowWidth, height: WindowHeight },
+    args: [`--window-size=${WindowWidth},${WindowHeight}`],
+  });
 
-// Browse to multiple Subreddits and scrape files
-try {
-  for (const [label, scrapeUrls] of Object.entries(SubReddits)) {
-    for (let i = 0; i < scrapeUrls.length && count < args.count; i++) {
-      // Navigate to the page and wait for network traffic to settle
-      const scrapeUrl = scrapeUrls[i];
-      console.log(y`Navigating to ${scrapeUrl}`);
-      await page.goto(scrapeUrl, { waitUntil: 'networkidle2' });
+  // Browse to multiple Subreddits and scrape files
+  try {
+    for (const [label, scrapeUrls] of Object.entries(SubReddits)) {
+      for (let i = 0; i < scrapeUrls.length && count < args.count; i++) {
+        const page = await browser.newPage();
+        await page.setUserAgent(ChromeUA);
 
-      // Wait for the loader to appear so we know the posts will load.
-      const retry = withRetry(RetryLimit, RedditErrorDelay);
-      await retry(
-        async () => {
-          await page.waitForSelector(LoaderSelector);
-          console.log(g`Finished loading ${scrapeUrl}`);
-        },
-        async () => {
-          console.log(r`Subreddit loading failed, refreshing`);
-          await page.reload({ waitUntil: 'networkidle2' });
-        }
-      );
+        const client = await page.createCDPSession();
+        await client.send('HeapProfiler.enable');
 
-      // Start scrapping images and scrolling through the page
-      while (true) {
-        // Get the image sources
-        const sources = await page.$$eval(ImageSelector, (images) =>
-          images.map(({ src }) => src)
-        );
+        // Navigate to the page and wait for network traffic to settle
+        const scrapeUrl = scrapeUrls[i];
+        console.log(y`Navigating to ${scrapeUrl}`);
+        await page.goto(scrapeUrl, { waitUntil: 'networkidle2' });
 
-        // Replace the preview urls with full image urls
-        const urls = sources.map((src) => {
-          const { pathname } = new URL(src);
-          const fileName = basename(pathname);
-          const shortFileName = fileName.substring(
-            fileName.lastIndexOf('-') + 1
-          );
-          return new URL(`https://i.redd.it/${shortFileName}`);
-        });
-
-        // Queue image uploads to bulk upload to Hugging Face, skip existing files
-        for (let i = 0; i < urls.length && count < args.count; i++) {
-          const origin = urls[i];
-
-          // Skip urls to images that have already been scrapped
-          if (scrappedUrls.has(origin.toString())) continue;
-          scrappedUrls.add(origin.toString());
-
-          queueValidation(origin, label);
-          if (validationQueue.size >= UploadBatchSize) {
-            await queueUpload();
-          }
-
-          if (pendingUploads.size >= MaxPendingUploads) {
-            await throttleUploads();
-          }
-        }
-
-        // Break if we've reached the maximum number of images
-        if (count >= args.count) {
-          console.log(y`Reached maximum image count`);
-          break;
-        }
-
-        // Check if the post loader is gone
-        // If so break, we've reached the end of the Subreddit
-        const loader = await page.$(LoaderSelector);
-        if (!loader) {
-          console.log(y`Reached end of ${scrapeUrl}`);
-          break;
-        }
-
-        // Delay a bit before scrolling to avoid rate-limiting
-        await wait(ScrollDelay);
-
-        // Clean up the downloaded images from the page to save memory
-        // Scroll to load more images
-        await page.evaluate(
-          (selector, remainder) => {
-            const elements = [...document.querySelectorAll(selector)];
-            for (const element of elements.slice(0, -remainder))
-              element.remove();
-            window.scrollBy(0, document.body.scrollHeight);
+        // Wait for the loader to appear so we know the posts will load.
+        const retry = withRetry(RetryLimit, RedditErrorDelay);
+        await retry(
+          async () => {
+            await page.waitForSelector(LoaderSelector);
+            console.log(g`Finished loading ${scrapeUrl}`);
           },
-          ...[CleanupSelector, CleanupRemainder]
-        );
-
-        // Click the retry button if an errors has occurred
-        if (await page.$(RetrySelector)) {
-          await wait(RedditErrorDelay);
-          const retryButton = await page.$(RetrySelector);
-          await retryButton?.click();
-        }
-
-        // Wait for the loader to disappear and the posts to finish loading
-        const loading = await page.$(LoadingSelector);
-        if (await loading?.isVisible()) {
-          try {
-            await waitForHidden(loading, LoadStuckTimeout);
-          } catch (error) {
-            console.warn(rl`Post loading failed, refreshing ${error}`);
+          async () => {
+            console.log(r`Subreddit loading failed, refreshing`);
             await page.reload({ waitUntil: 'networkidle2' });
           }
+        );
+
+        // Start scrapping images and scrolling through the page
+        while (true) {
+          // Get the image sources
+          const sources = await page.$$eval(ImageSelector, (images) =>
+            images.map(({ src }) => src)
+          );
+
+          // Replace the preview urls with full image urls
+          const urls = sources.map((src) => {
+            const { pathname } = new URL(src);
+            const fileName = basename(pathname);
+            const shortFileName = fileName.substring(
+              fileName.lastIndexOf('-') + 1
+            );
+            return new URL(`https://i.redd.it/${shortFileName}`);
+          });
+
+          // Queue image uploads to bulk upload to Hugging Face, skip existing files
+          for (let i = 0; i < urls.length && count < args.count; i++) {
+            const origin = urls[i];
+
+            // Skip urls to images that have already been scrapped
+            if (scrappedUrls.has(origin.toString())) continue;
+            scrappedUrls.add(origin.toString());
+
+            queueValidation(origin, label);
+            if (validationQueue.size >= UploadBatchSize) {
+              await queueUpload();
+            }
+
+            if (pendingUploads.size >= MaxPendingUploads) {
+              await throttleUploads();
+            }
+          }
+
+          // Break if we've reached the maximum number of images
+          if (count >= args.count) {
+            console.log(y`Reached maximum image count`);
+            break;
+          }
+
+          // Check if the post loader is gone
+          // If so break, we've reached the end of the Subreddit
+          const loader = await page.$(LoaderSelector);
+          if (!loader) {
+            console.log(y`Reached end of ${scrapeUrl}`);
+            break;
+          }
+
+          // Delay a bit before scrolling to avoid rate-limiting
+          await wait(ScrollDelay);
+
+          // Scroll to load more images
+          await page.evaluate(() =>
+            window.scrollBy(0, document.body.scrollHeight)
+          );
+
+          // Click the retry button if an errors has occurred
+          if (await page.$(RetrySelector)) {
+            await wait(RedditErrorDelay);
+            const retryButton = await page.$(RetrySelector);
+            await retryButton?.click();
+          }
+
+          // Force garbage collection after removing extra elements.
+          await client.send('HeapProfiler.collectGarbage');
+
+          // Wait for the loader to disappear and the posts to finish loading
+          const loading = await page.$(LoadingSelector);
+          if (await loading?.isVisible()) {
+            try {
+              await waitForHidden(loading, LoadStuckTimeout);
+            } catch (error) {
+              console.warn(rl`Post loading failed, refreshing ${error}`);
+              await page.reload({ waitUntil: 'networkidle2' });
+            }
+          }
         }
+
+        // Close the page to return the used memory
+        await page.close();
       }
     }
+  } catch (error) {
+    // Make sure the log directory exists
+    await mkdir(LogPath, { recursive: true });
+
+    // Log the last stack trace
+    const errorLog = join(LogPath, 'error.log');
+    await writeFile(errorLog, error.stack);
+
+    // Log the last screenshot
+    const errorPng = join(LogPath, 'error.png');
+    await page.screenshot({ path: errorPng });
+
+    console.error(r`Unexpected failure ${error}`);
+    throw error;
   }
-} catch (error) {
-  // Make sure the log directory exists
-  await mkdir(LogPath, { recursive: true });
 
-  // Log the last stack trace
-  const errorLog = join(LogPath, 'error.log');
-  await writeFile(errorLog, error.stack);
-
-  // Log the last screenshot
-  const errorPng = join(LogPath, 'error.png');
-  await page.screenshot({ path: errorPng });
-
-  throw error;
+  // Close the browser to release some memory
+  console.log(g`Done with all subreddits`);
+  await browser.close();
 }
 
 // Upload the remaining files to Hugging Face
@@ -351,9 +359,6 @@ if (validationQueue.size) {
 
 // Wait for all pending uploads to finish
 await Promise.all([...pendingUploads]);
-
-// Close browser and finish
-await browser.close();
 console.log(g`Done!`);
 
 /**
