dba88e23cd0093e202c61510e8958c03ea7b37ac
Sloan Haywood <haywood.sloan@yahoo.com>
2025-05-29 12:53:13 -0400

    limit log analytics for cost savings

--- infra/modules/global/insights/main.tf
+++ infra/modules/global/insights/main.tf
@@ -8,6 +8,10 @@ resource "azurerm_application_insights" "insights" {
   name                = "app-insights"
   resource_group_name = var.rg_name
   location            = var.region_name
+
   workspace_id        = azurerm_log_analytics_workspace.workspace.id
   application_type    = "Node.JS"
+
+  daily_data_cap_in_gb = 1
+  sampling_percentage = 25
 }
