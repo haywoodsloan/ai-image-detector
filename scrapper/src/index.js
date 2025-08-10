36735e409973149faf9b426cb743693f54c09bae
Sloan Haywood <haywood.sloan@gmail.com>
2025-08-10 03:24:48 -0400

    scrapper uses a new page on each retry

--- scrapper/src/index.js
+++ scrapper/src/index.js
@@ -243,6 +243,9 @@ if (args.real || args.all) {
   /** @type {Page} */
   let page;
 
+  /** @type {CDPSession} */
+  let client;
+
   // Browse to multiple Subreddits and scrape files
   const toScrape = shuffle([
     ...(args.real || args.all ? RealSubReddits.map((s) => [RealLabel, s]) : []),
@@ -250,25 +253,29 @@ if (args.real || args.all) {
   ]);
 
   try {
+    const retry = withRetry(RetryLimit, RedditErrorDelay);
     for (const [label, scrapeUrl] of toScrape) {
-      page = await browser.newPage();
-      await page.setUserAgent(ChromeUA);
-
-      const client = await page.createCDPSession();
-      await client.send('HeapProfiler.enable');
-
       // Navigate to the page and wait for network traffic to settle
       console.log(y`Navigating to ${scrapeUrl}`);
 
       // Wait for the loader to appear so we know the posts will load.
-      const retry = withRetry(RetryLimit, RedditErrorDelay);
       await retry(
         async () => {
+          page = await browser.newPage();
+          await page.setUserAgent(ChromeUA);
+
+          client = await page.createCDPSession();
+          await client.send('HeapProfiler.enable');
+
           await page.goto(scrapeUrl, { waitUntil: 'networkidle2' });
           await page.waitForSelector(LoaderSelector);
+
           console.log(g`Finished loading ${scrapeUrl}`);
         },
-        async () => console.log(r`Subreddit loading failed, refreshing`)
+        async () => {
+          console.log(r`Subreddit loading failed, refreshing`);
+          await page.close()
+        }
       );
 
       // Start scrapping images and scrolling through the page
--- scrapper/src/types.d.ts
+++ scrapper/src/types.d.ts
@@ -2,3 +2,5 @@ declare type ElementHandle<T extends Node = Element> =
   import('puppeteer').ElementHandle<T>;
 
 declare type Page = import('puppeteer').Page;
+
+declare type CDPSession = import('puppeteer').CDPSession;
